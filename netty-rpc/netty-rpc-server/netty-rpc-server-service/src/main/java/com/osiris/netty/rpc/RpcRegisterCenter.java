37a3e397966776e0c52ab8a0df38dad7647574ec
lijia <502766817@qq.com>
2020-07-13 11:07:21 +0800

    upd

--- netty-rpc/netty-rpc-server/netty-rpc-server-service/src/main/java/com/osiris/netty/rpc/RpcRegisterCenter.java
+++ netty-rpc/netty-rpc-server/netty-rpc-server-service/src/main/java/com/osiris/netty/rpc/RpcRegisterCenter.java
@@ -46,16 +46,15 @@ public class RpcRegisterCenter implements ApplicationContextAware, InitializingB
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
-                    protected void initChannel(SocketChannel ch) throws Exception {
+                    protected void initChannel(SocketChannel ch) {
                         ChannelPipeline pipeline = ch.pipeline();
                         //自定义协议解码器
-                        /** 入参有 5 个， 分别解释如下
-                         maxFrameLength： 框架的最大长度。 如果帧的长度大于此值， 则将抛出 TooLongFrameException。
-                         lengthFieldOffset： 长度字段的偏移量： 即对应的长度字段在整个消息数据中得位置
-                         lengthFieldLength： 长度字段的长度。 如： 长度字段是 int 型表示， 那么这个值就是 4（long 型就是 8）
-                         lengthAdjustment： 要添加到长度字段值的补偿值
-                         initialBytesToStrip： 从解码帧中去除的第一个字节数
-                         */
+                        // 入参有 5 个， 分别解释如下
+                        //maxFrameLength： 框架的最大长度。 如果帧的长度大于此值， 则将抛出 TooLongFrameException。
+                        //lengthFieldOffset： 长度字段的偏移量： 即对应的长度字段在整个消息数据中得位置
+                        //lengthFieldLength： 长度字段的长度。 如： 长度字段是 int 型表示， 那么这个值就是 4（long 型就是 8）
+                        //lengthAdjustment： 要添加到长度字段值的补偿值
+                        //initialBytesToStrip： 从解码帧中去除的第一个字节数
                         pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
                         //自定义协议编码器
                         pipeline.addLast(new LengthFieldPrepender(4));
